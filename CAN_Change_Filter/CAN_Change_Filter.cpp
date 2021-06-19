194107289ad0bcacf1852cdc66d074eb0399dc63
Brandon Van Pelt <brandonvanpelt@outlook.com>
2021-06-18 21:32:38 -0500

    work

--- CAN_Change_Filter/CAN_Change_Filter.cpp
+++ CAN_Change_Filter/CAN_Change_Filter.cpp
@@ -10,8 +10,19 @@
 #include <stdlib.h> 
 #include <string>
 
-#define MAXLINE 250 //every line with end with \n 
+/*=========================================================
+    END DEBUGGING
+===========================================================*/
+//#define DEBUG_FINDIDS
+#define DEVMODE
+#define DEBUG_XOR
+/*=========================================================
+    END DEBUGGING
+===========================================================*/
+
+#define MAXLINE 200 
 #define MAX_STRING_LENGTH 100
+
 using namespace std;
 
 list<int> IDBuffer;
@@ -23,87 +34,59 @@ typedef struct
     int id;
     int length;
     int data[8];
-}CANMSG;
+} CANMSG;
 
-int inputLine(FILE* fp, CANMSG e)
-{
-    return 1;
-}
+CANMSG buffer;
 
-void parseFile(FILE* fptr, char* delimiter) 
+bool isIDInList(int id)
 {
-    char buffer[MAXLINE] = { 0 };
-    char* linestatus = NULL;
-    char* token = NULL;
-    int count = 0;
-    while ((linestatus = fgets(buffer, MAXLINE, fptr)) != NULL) {
-        int len = strlen(buffer);
-        buffer[len - 2] = '\0'; //removing \n from string   
-        token = strtok(buffer, " ");
-        count = 0;
-        while (token != NULL) 
-        {
-            printf("%s  ", token); //or store  
-            token = strtok(NULL, delimiter);
-            count++;
-        }
-        printf("\n");
-    }
+    return std::find(IDBuffer.begin(), IDBuffer.end(), id) != IDBuffer.end();
 }
 
 int findIDs(char* filename)
 {
-    CANMSG buff[100];
-    FILE* fp; // declaration of file pointer
-    char buffer[1000]; // variable to read the content
-    fopen_s(&fp, filename, "r");// opening of file
-    if (!fp)// checking for error
-        return 1;
-
-    char s[2] = " ";
-    //parseFile(fp, s);
-    
-    inputLine(fp, buff[0]);
-    printf("%h", buff[0].id);
-
-    while (fgets(buffer, 1000, fp) != NULL)
+    FILE* ptr = fopen(filename, "r");
+    if (ptr == NULL)
     {
-      
+        printf("no such file.");
+        return 0;
     }
-    //    printf("%s", buffer);
-    
 
-    fclose(fp); // closing file
-    return 0;
-}
+    // Msg Num, Time, ID, Length
+    while (fscanf(ptr, "%*s %*s %x %d", &buffer.id, &buffer.length) != EOF)
+    {
+        #if defined DEBUG_FINDIDS
+            printf("%x\n", buffer.id);
+        #endif
+        
+        // Determine length and loop the the data array, an array shorter than 8 is padded with zeros
+        for (int i = 0; i < 8; i++)
+        {
+            (i < buffer.length) ? (fscanf(ptr, "%x", &buffer.data[i])) : (buffer.data[i] = 0);
 
-bool isIDInList(int id)
-{
-    // Check if an element exists in list
-    // Create a list Iterator
-    list<int>::iterator it;
-    // Fetch the iterator of element with value 'the'
-    it = find(IDBuffer.begin(), IDBuffer.end(), id);
-    // Check if iterator points to end or not
-    if (it != IDBuffer.end())
-    {
-        // It does not point to end, it means element exists in list
-        cout << id + " exists in list " << endl;
-        return true;
-    }
-    else
-    {
-        // It points to end, it means element does not exists in list
-        cout << id +" does not exists in list" << endl;
-        return false;
+        #if defined DEBUG_FINDIDS
+            printf("%x ", buffer.data[i]);
+        #endif
+
+        }
+
+        #if defined DEBUG_FINDIDS
+            printf("\n");
+        #endif
+        
+        // Add id to list if not already
+        if (!isIDInList(buffer.id))
+        {
+            IDBuffer.push_back(buffer.id);
+        }
     }
 }
 
+// Find the length of a file
 int fileLength(char* filename) {
     int line_count = 0;
     FILE* fp;
     char str[MAX_STRING_LENGTH];
-    //	char *filename = nodeinfo_location;
     fopen_s(&fp, filename, "r");
     if (fp == NULL) {
         printf("Could not open file %s", filename);
@@ -113,17 +96,29 @@ int fileLength(char* filename) {
     while (fgets(str, MAX_STRING_LENGTH, fp) != NULL) {
         line_count++;
     }
-    // Value returned is used to determine structure size
     return line_count;
 }
 
-int main(int argc, char* argv[])
+// Call with false if you just want count
+int printIDList(bool printMe = true)
 {
-    CANMSG buffer;
-    char filename[] = "AutoStopStart01.txt";
+    int count = 0;
+    IDBuffer.sort();
 
-    printf("File length: %d\n", fileLength(filename));
-   
+    for (int x : IDBuffer)
+    {
+        if (printMe)
+        {
+            printf("%x\n", x);
+        }
+        count++;
+    }
+    return count;
+}
+
+
+int XORData(char* filename, int id)
+{
     FILE* ptr = fopen(filename, "r");
     if (ptr == NULL)
     {
@@ -133,24 +128,50 @@ int main(int argc, char* argv[])
 
     while (fscanf(ptr, "%*s %*s %x %d", &buffer.id, &buffer.length) != EOF)
     {
-        //printf("%x\n", buffer.id);
-        //printf("%d\n", buffer.length);
-
+#if defined DEBUG_XOR
+        if (buffer.id == 83 && id == 83)
+        {
+            printf("ID: %x\n", buffer.id);
+            printf("Length: %x\n", buffer.length);
+        }
+#endif
         for (int i = 0; i < 8; i++)
         {
             (i < buffer.length) ? (fscanf(ptr, "%x", &buffer.data[i])) : (buffer.data[i] = 0);
+#if defined DEBUG_XOR
             //printf("%x ", buffer.data[i]);
+#endif
         }
+#if defined DEBUG_XOR
         //printf("\n");
-        if (!isIDInList(buffer.id))
-        {
-            //IDBuffer.push_back(buffer.id);
-        }
+#endif
     }
-    IDBuffer.sort();
+}
 
-    for (int x : IDBuffer)
+void startXOR(char* filename)
+{
+    for (int id : IDBuffer)
     {
-        printf("%x\n", x);
+        printf("XOR ID: %x\n ", id);
+        XORData(filename, id);
     }
+}
+
+// XOR: ^
+int main(int argc, char* argv[])
+{
+#if defined DEVMODE
+    char filename[] = "AutoStopStart01.txt";
+#else 
+    char filename[] = argv[1];
+#endif
+    
+    printf("File length: %d\n", fileLength(filename));
+
+    findIDs(filename);
+
+    printf("Unique IDs: %d\n", printIDList(false));
+    
+    startXOR(filename);
+
 }
